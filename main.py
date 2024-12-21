eef1b70a321905a00f3d9afe7226c1cb6557292e
Todne <calvin.brockie@gmail.com>
2024-12-21 00:46:43 -0700

    refactor to use updatable and drawable groups

--- main.py
+++ main.py
@@ -4,21 +4,28 @@ from player import Player
 
 def main():
     pygame.init()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     timer = pygame.time.Clock()
     dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        for object in updatable:
+            object.update(dt)
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = timer.tick(60) / 1000
 
