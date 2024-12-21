a3147af11694479e0fbde26feda32b4cb98341cc
Todne <calvin.brockie@gmail.com>
2024-12-21 00:04:25 -0700

    add timer to limit fps

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    timer = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,6 +16,7 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = timer.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
