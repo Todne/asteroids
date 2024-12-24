f6805561cdbed12d8ef83af12d70652974be640b
Todne <calvin.brockie@gmail.com>
2024-12-23 17:30:56 -0700

    add logic to kill shots after a time

--- shot.py
+++ shot.py
@@ -1,13 +1,17 @@
 import circleshape
 import pygame
-from constants import SHOT_RADIUS
+from constants import SHOT_RADIUS, PLAYER_SHOOT_LIFETIME
 
 class Shot(circleshape.CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
+        self.shot_lifetime = PLAYER_SHOOT_LIFETIME
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
         self.position += self.velocity * dt
+        self.shot_lifetime -= dt
+        if self.shot_lifetime <= 0:
+            self.kill()
